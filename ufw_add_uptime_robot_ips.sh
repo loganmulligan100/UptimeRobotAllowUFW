93e2f82f88baf723980d52a2d0e956b4b7595be9
loganmulligan100 <66179086+loganmulligan100@users.noreply.github.com>
2024-06-22 14:39:42 -0700

    Update ufw_add_uptime_robot_ips.sh

--- ufw_add_uptime_robot_ips.sh
+++ ufw_add_uptime_robot_ips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # URL to fetch the list of Uptime Robot IP addresses
-UPTIME_ROBOT_IPS_URL="https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt"
+UPTIME_ROBOT_IPS_URL="https://uptimerobot.com/inc/files/ips/IPv4.txt"
 
 # Temporary file to store the list of IP addresses
 TEMP_IP_LIST="/tmp/uptimerobot_ips.txt"
@@ -21,7 +21,7 @@ while read -r ip; do
     ip=$(echo $ip | xargs)
     
     # Validate the IP address format (both IPv4 and IPv6 with CIDR)
-    if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}(/[0-9]{1,2})?$ ]] || [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}(/[0-9]{1,3})?$ ]]; then
+    if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}(/[0-9]{1,2})?$ ]] || [[ $ip =~ ^([0-9a-fA-F]{0,4}:){2,7}([0-9a-fA-F]{0,4})?(/[0-9]{1,3})?$ ]]; then
         sudo ufw allow from $ip comment "Uptime Robot"
         if [ $? -ne 0 ]; then
             echo "Failed to add IP: $ip"
