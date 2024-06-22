0ac8c504582b552a2c4f81c56e3005a479d66f07
loganmulligan100 <66179086+loganmulligan100@users.noreply.github.com>
2024-06-22 15:15:29 -0700

    Update ufw_add_uptime_robot_ips.sh
    QOL improvements as well as purge work

--- ufw_add_uptime_robot_ips.sh
+++ ufw_add_uptime_robot_ips.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # URL to fetch the list of Uptime Robot IP addresses
 UPTIME_ROBOT_IPS_URL="https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt"
@@ -11,71 +11,65 @@ ufw_ignored=0
 ufw_add_ip () {
     if [ ! -z "$1" ]; then
         rule=$(LC_ALL=C sudo ufw allow from $1 comment "Uptime Robot")
-
-        if [ "$rule" = 'Rule added' ] || [ "$rule" = 'Rule added (v6)' ]; then
+        if [[ "$rule" == *"Rule added"* ]]; then
             ufw_created=$((ufw_created+1))
             return
         fi
     fi
-
-    ufw_ignored=$((ufw_ignored+1))
-}
-
-ufw_delete_ip () {
-    if [ ! -z "$1" ]; then
-        rule=$(LC_ALL=C sudo ufw delete allow from $1)
-
-        if [ "$rule" = 'Rule deleted' ] || [ "$rule" = 'Rule deleted (v6)' ]; then
-            ufw_deleted=$((ufw_deleted+1))
-            return
-        fi
-    fi
-
     ufw_ignored=$((ufw_ignored+1))
 }
 
 ufw_purge_rules () {
     while IFS= read -r line; do
         if echo "$line" | grep -q '# Uptime Robot'; then
-            number=$(echo "$line" | awk '{print $1}' | tr -d '[]')
+            number=$(echo "$line" | grep -o '^\[ *[0-9]*\]' | tr -d '[] ')
             sudo ufw --force delete "$number"
             ufw_deleted=$((ufw_deleted+1))
-            show_progress "$number" "$ufw_deleted" "$ufw_created" "$ufw_ignored"
         fi
     done < <(sudo ufw status numbered)
 }
 
 show_progress() {
     local current=$1
-    local deleted=$2
-    local created=$3
-    local ignored=$4
-    local progress=$((current * 100 / total_ips))
+    local total=$2
+    local deleted=$3
+    local created=$4
+    local ignored=$5
+    local progress=$((current * 100 / total))
     local done=$((progress * 4 / 10))
     local left=$((40 - done))
     local fill=$(printf "%${done}s")
     local empty=$(printf "%${left}s")
-    printf "\rProgress: [${fill// /#}${empty// /-}] ${progress}%% - Deleted: $deleted, Created: $created, Ignored: $ignored"
+    printf "\rProgress: [${fill// /#}${empty// /-}] ${progress}%%"
+    echo -e "\n\033[32mCreated: $created\033[0m"
+    echo -e "\033[33mIgnored: $ignored\033[0m"
+    echo -e "\033[31mDeleted: $deleted\033[0m"
 }
 
-if [ "$1" = "--purge" ]; then
+if [ "$1" == "--purge" ]; then
     ufw_purge_rules
-else
-    ips=$(curl -s $UPTIME_ROBOT_IPS_URL | tr '\r' '\n' | tr -s '\n')
-    total_ips=$(echo "$ips" | wc -l)
-    current_ip=0
-
-    for ip in $ips; do
-        ufw_add_ip "$ip"
-        current_ip=$((current_ip + 1))
-        show_progress $current_ip $ufw_deleted $ufw_created $ufw_ignored
-    done
-    echo ""
-    sudo ufw reload
+    echo ''
+    echo -e "\033[32mTotal rules created: ${ufw_created}\033[0m"
+    echo -e "\033[33mTotal rules ignored: ${ufw_ignored}\033[0m"
+    echo -e "\033[31mTotal rules deleted: ${ufw_deleted}\033[0m"
+    echo 'Done.'
+    exit 0
 fi
 
+ips=$(curl -s $UPTIME_ROBOT_IPS_URL | tr '\r' '\n' | tr -s '\n')
+total_ips=$(echo "$ips" | wc -l)
+current_ip=0
+
+for ip in $ips; do
+    ufw_add_ip "$ip"
+    current_ip=$((current_ip + 1))
+    show_progress $current_ip $total_ips $ufw_deleted $ufw_created $ufw_ignored
+done
+echo ""
+sudo ufw reload
+
 echo ''
-echo "Total rules deleted: ${ufw_deleted}"
-echo "Total rules created: ${ufw_created}"
-echo "Total rules ignored: ${ufw_ignored}"
+echo -e "\033[32mTotal rules created: ${ufw_created}\033[0m"
+echo -e "\033[33mTotal rules ignored: ${ufw_ignored}\033[0m"
+echo -e "\033[31mTotal rules deleted: ${ufw_deleted}\033[0m"
 echo 'Done.'
