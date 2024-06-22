3081dfdab000712043ef02e9dc445a455a10086c
loganmulligan100 <66179086+loganmulligan100@users.noreply.github.com>
2024-06-22 15:07:17 -0700

    Update ufw_add_uptime_robot_ips.sh
    Final Versions

--- ufw_add_uptime_robot_ips.sh
+++ ufw_add_uptime_robot_ips.sh
@@ -13,13 +13,11 @@ ufw_add_ip () {
         rule=$(LC_ALL=C sudo ufw allow from $1 comment "Uptime Robot")
 
         if [ "$rule" = 'Rule added' ] || [ "$rule" = 'Rule added (v6)' ]; then
-            printf "+"
             ufw_created=$((ufw_created+1))
             return
         fi
     fi
 
-    printf "."
     ufw_ignored=$((ufw_ignored+1))
 }
 
@@ -28,13 +26,11 @@ ufw_delete_ip () {
         rule=$(LC_ALL=C sudo ufw delete allow from $1)
 
         if [ "$rule" = 'Rule deleted' ] || [ "$rule" = 'Rule deleted (v6)' ]; then
-            printf "-"
             ufw_deleted=$((ufw_deleted+1))
             return
         fi
     fi
 
-    printf "."
     ufw_ignored=$((ufw_ignored+1))
 }
 
@@ -54,6 +50,17 @@ ufw_purge_rules () {
     done
 }
 
+show_progress() {
+    local current=$1
+    local total=$2
+    local progress=$((current * 100 / total))
+    local done=$((progress * 4 / 10))
+    local left=$((40 - done))
+    local fill=$(printf "%${done}s")
+    local empty=$(printf "%${left}s")
+    printf "\rProgress: [${fill// /#}${empty// /-}] ${progress}%%"
+}
+
 if [ "$1" = "--purge" ]; then
     ufw_purge_rules
 else
@@ -64,7 +71,7 @@ else
     for ip in $ips; do
         ufw_add_ip "$ip"
         current_ip=$((current_ip + 1))
-        printf "\rAdding IPs: %d/%d" $current_ip $total_ips
+        show_progress $current_ip $total_ips
     done
     echo ""
     sudo ufw reload
@@ -75,6 +82,3 @@ echo "Total rules deleted: ${ufw_deleted}"
 echo "Total rules created: ${ufw_created}"
 echo "Total rules ignored: ${ufw_ignored}"
 echo 'Done.'
-
-# Clean up by removing the temporary IP list file
-rm -f $TEMP_IP_LIST
