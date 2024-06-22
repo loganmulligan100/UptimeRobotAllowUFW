965c5e34b2d25f0606455f99d85c612353cf1009
loganmulligan100 <66179086+loganmulligan100@users.noreply.github.com>
2024-06-22 14:35:00 -0700

    Update ufw_add_uptime_robot_ips.sh
    Another Test Commit

--- ufw_add_uptime_robot_ips.sh
+++ ufw_add_uptime_robot_ips.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-# URL of the IP list from Uptime Robot
-IP_LIST_URL="https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt"
-TEMP_IP_FILE="uptime_robot_ips.txt"
+# URL to fetch the list of Uptime Robot IP addresses
+UPTIME_ROBOT_IPS_URL="https://uptimerobot.com/inc/files/ips/IPv4.txt"
 
-# Download the IP list
-curl -s $IP_LIST_URL -o $TEMP_IP_FILE
+# Temporary file to store the list of IP addresses
+TEMP_IP_LIST="/tmp/uptimerobot_ips.txt"
 
-# Check if the file was downloaded
-if [[ ! -f "$TEMP_IP_FILE" ]]; then
-    echo "Failed to download the IP list from Uptime Robot!"
+# Fetch the list of IP addresses
+curl -s $UPTIME_ROBOT_IPS_URL -o $TEMP_IP_LIST
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to download the list of Uptime Robot IP addresses."
     exit 1
 fi
 
-# Read and add each IP to UFW
-while IFS= read -r ip; do
-    if [[ ! -z "$ip" ]]; then
-        sudo ufw allow from $ip
-        echo "Allowed IP: $ip"
+# Read each IP address from the downloaded list and add it to UFW rules
+while read -r ip; do
+    # Trim any leading or trailing whitespace from the IP address
+    ip=$(echo $ip | xargs)
+    
+    # Validate the IP address format
+    if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        sudo ufw allow from $ip comment "Uptime Robot"
+        if [ $? -ne 0 ]; then
+            echo "Failed to add IP: $ip"
+        else
+            echo "Added IP: $ip"
+        fi
+    else
+        echo "Invalid IP address format: $ip"
     fi
-done < "$TEMP_IP_FILE"
+done < $TEMP_IP_LIST
 
-# Reload UFW to apply changes
+# Reload UFW to apply the new rules
 sudo ufw reload
-echo "UFW rules updated and reloaded."
 
-# Clean up
-rm $TEMP_IP_FILE
+# Clean up by removing the temporary IP list file
+rm -f $TEMP_IP_LIST
+
+echo "Uptime Robot IP addresses have been added to UFW rules."
